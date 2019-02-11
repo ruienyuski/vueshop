15dbd4583e61d752a51b96f551fe08ab395cf18f
yuski.tw@gmail.com <yuski.tw@gmail.com>
2019-02-11 12:04:03 +0800

    update Index.vue & main.js & bus.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
 import axios from 'axios';
 import VueAxios from 'vue-axios';
@@ -10,26 +10,26 @@ import 'vue-loading-overlay/dist/vue-loading.css';
 import VeeValidate from 'vee-validate';
 import zhTWValidate from 'vee-validate/dist/locale/zh_TW';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 import './bus';
 import dateFilter from './filters/date';
-import currencyFilter from'./filters/currency';
+import currencyFilter from './filters/currency';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
 Vue.use(VeeValidate);
-VeeValidate.Validator.localize('zh_TW',zhTWValidate);
+VeeValidate.Validator.localize('zh_TW', zhTWValidate);
 
 Vue.component('Loading', Loading);
-Vue.filter('currency',currencyFilter);
-Vue.filter('date',dateFilter);
+Vue.filter('currency', currencyFilter);
+Vue.filter('date', dateFilter);
 
 axios.defaults.withCredentials = true;
 
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
